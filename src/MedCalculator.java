6424920e4c7b57328272b0e6e15d9b4c13366f27
dowglasOficial <bittencourtdouglas41@gmail.com>
2025-01-21 20:12:21 -0300

    subindo o projeto

---
+++ src/MedCalculator.java
@@ -0,0 +1,52 @@
+public class MedCalculator {
+    private float noteOne;
+    private float noteTwo;
+    private float noteTree;
+    private float noteFor;
+
+    public float getNoteOne() {
+        return noteOne;
+    }
+
+    ;
+
+    public void setNoteOne(float noteOne) {
+        this.noteOne = noteOne;
+    }
+
+    ;
+
+    public float getNoteTwo() {
+        return noteTwo;
+    }
+
+    ;
+
+    public void setNoteTwo(float noteTwo) {
+        this.noteTwo = noteTwo;
+    }
+
+    public float getNoteTree() {
+        return noteTree;
+    }
+
+    ;
+
+    public void setNoteTree(float noteTree) {
+        this.noteTree = noteTree;
+    }
+
+    ;
+
+    public float getNoteFor() {
+        return noteFor;
+    }
+
+    ;
+
+    public void setNoteFor(float noteFor) {
+        this.noteFor = noteFor;
+    }
+
+    ;
+}
